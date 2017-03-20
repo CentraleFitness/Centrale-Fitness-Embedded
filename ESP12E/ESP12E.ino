630c3f94f21ab556cf9ec17b74b965a9908557d0
Léo POULIN <leoraphael.poulin@epitech.eu>
2017-03-20 23:45:55 +0100

    Wifi connect to AP

--- ESP12E/ESP12E.ino
+++ ESP12E/ESP12E.ino
@@ -4,12 +4,42 @@
  Author:	Kyalma
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
 
+#include <WiFiUdp.h>
+#include <WiFiServer.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiType.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFi.h>
+
+// Network hosted on the WRT54GS Router
+
+#define SSID	"centralefitness"
+#define	PASS	"epitechforward42A"
+
+ESP8266WiFiClass	g_wifi;
+wl_status_t			g_status;
+
+void setup() {
+	Serial.begin(9600);
+	Serial.println("Boot");
+
+	g_wifi.mode(WIFI_STA);
+	g_wifi.begin(SSID, PASS);
+	while (g_wifi.status() != WL_CONNECTED) {
+		delay(100);
+	}
+	g_status = g_wifi.status();
+	Serial.println("IP Adress: " + g_wifi.localIP().toString());
+	g_wifi.setAutoConnect(true);
+	Serial.println("Setup finihed");
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+	delay(1000);
 }
